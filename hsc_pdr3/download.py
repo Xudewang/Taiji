3ad2b5de3424e33828cb6746056dd4fd8c1fe037
Dewang Xu <dwxu.astro@gmail.com>
2022-09-12 14:24:09 +0800

    Update download.py
    delete the submit job

--- hsc_pdr3/download.py
+++ hsc_pdr3/download.py
@@ -92,8 +92,7 @@ def hsc_query_tool(sql_file, catalog_file, dr_type, data_path, code_path):
         print('The sql exists, we remove it.')
         os.remove(catalog_file)
 
-    process_sql = subprocess.Popen(["python /home/dewang/Taiji/hsc_pdr3/hscReleaseQuery/hscReleaseQuery.py --user='dwxu' --release-version={} --nomail --skip-syntax-check \
-                                    {} --format csv > {}".format(dr_type, sql_file, catalog_file)], shell=True)
+    process_sql = subprocess.Popen(["python /home/dewang/Taiji/hsc_pdr3/hscReleaseQuery/hscReleaseQuery.py --user='dwxu' --release-version={} --nomail --delete-job --skip-syntax-check {} --format csv > {}".format(dr_type, sql_file, catalog_file)], shell=True)
 
     return_code_sql = process_sql.wait()
     
