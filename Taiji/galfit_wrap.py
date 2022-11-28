fed0b340bb4995e808e568ed7feaa0a54f60a0d4
Dewang Xu <dwxu.astro@gmail.com>
2022-11-28 16:33:07 +0800

    chagne the galfit name, and add the mask_constrain

--- Taiji/galfit_wrap.py
+++ Taiji/galfit_wrap.py
@@ -48,6 +48,77 @@ def make_head(runoption_num,
     return head
 
 
+def Make_constrains(component_number, name_arr, constrain_dict,
+                    constrain_type):
+    """ To make the constrain file for Galfit. As the Galfit constrain file described, the parameters include x, y, mag, re, rs, n, alpha, beta, gamma, pa, q. Basically, two formats are useful.
+    
+    # The first type is:
+    3              n        0.7 to 5    # Soft constraint: Constrains the 
+                                        # sersic index n to within values 
+                                        # from 0.7 to 5.
+    
+    # The second type is:
+    2	  	   x	     -1  0.5	# Soft constraint: Constrains 
+                                    # x-position of component
+                                    # 2 to within +0.5 and -1 of the
+                                    # >>INPUT<< value.
+
+    Returns:
+        string: the constrain string for Galfit.
+    """
+
+    constrain = '# This is constrain file.'
+    for i in range(component_number):
+
+        if name_arr[i] == "expdisk1":
+            constrain  += '\n\n'
+            constrain += f'\n#  Component type: {name_arr[i]}; Component number: {i+1}'
+            constrain += '\n{:2d}        x        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['x_low'], constrain_type['x_cons'],
+                constrain_dict['x_high'])
+            constrain += '\n{:2d}        y        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['y_low'], constrain_type['y_cons'],
+                constrain_dict['y_high'])
+            constrain += '\n{:2d}        mag        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['mu0_low'], constrain_type['mu0_cons'],
+                constrain_dict['mu0_high'])
+            constrain += '\n{:2d}        rs        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['rs_low'], constrain_type['rs_cons'],
+                constrain_dict['rs_high'])
+            constrain += '\n{:2d}        q        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['q_exp_low'], constrain_type['q_exp_cons'],
+                constrain_dict['q_exp_high'])
+            constrain += '\n{:2d}        pa        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['pa_exp_low'], constrain_type['pa_exp_cons'],
+                constrain_dict['pa_exp_high'])
+            
+        if name_arr[i] == 'sersic2':
+            constrain += f'\n#  Component type: {name_arr[i]}; Component number: {i+1}'
+            constrain += '\n{:2d}        x        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['x_low'], constrain_type['x_cons'],
+                constrain_dict['x_high'])
+            constrain += '\n{:2d}        y        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['y_low'], constrain_type['y_cons'],
+                constrain_dict['y_high'])
+            constrain += '\n{:2d}        mag        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['mue_low'], constrain_type['mue_cons'],
+                constrain_dict['mue_high'])
+            constrain += '\n{:2d}        re        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['re_low'], constrain_type['re_cons'],
+                constrain_dict['re_high'])
+            constrain += '\n{:2d}        n        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['n_low'], constrain_type['n_cons'],
+                constrain_dict['n_high'])
+            constrain += '\n{:2d}        q        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['q_sersic_low'], constrain_type['q_sersic_cons'],
+                constrain_dict['q_sersic_high'])
+            constrain += '\n{:2d}        pa        {:8.4f} {} {:8.4f}'.format(
+                i + 1, constrain_dict['pa_sersic_low'], constrain_type['pa_sersic_cons'],
+                constrain_dict['pa_sersic_high'])
+
+    return constrain
+
+
 def Make_Sersic_parameters(value_arr, fixed_num_arr=np.array([1, 1, 1, 1, 1])):
 
     parameternumber_arr = [3, 4, 5, 9, 10]
@@ -179,7 +250,7 @@ def Galfit_fit(feedme_file, run_type, feedme_dir, code_dir=None):
 
     else:
         print('Galfit does not run!')
-        
+
     # if Path('galfit.01').exists():
     #     #Path('galfit.01').unlink()
     #     print('galfit.01 exsits')
