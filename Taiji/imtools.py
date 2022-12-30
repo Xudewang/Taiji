c99fea77df1d64c55708e8fb31a9ed4f55d5094e
Dewang Xu <dwxu.astro@gmail.com>
2022-12-30 17:45:42 +0800

    add photutils isophote fixed extraction written by Siyue.

--- Taiji/imtools.py
+++ Taiji/imtools.py
@@ -1392,8 +1392,8 @@ autocmap.set_under('k', alpha=0)
 def display_image_colornorm(ax, image, cmap='Greys_r', percentile=99.9, vmin=None, vmax=None, scale_bar=True,
                             bar_length=10, bar_fontsize=15,
                             pixel_scale=0.259, box_alpha=1, **kwargs):
-    from matplotlib.colors import LogNorm, Normalize
     from astropy.visualization import PercentileInterval
+    from matplotlib.colors import LogNorm, Normalize
 
     # Calculate the minimum and maximum values based on the central 99% of the data
     interval = PercentileInterval(percentile)
@@ -2984,6 +2984,57 @@ def Running_median(X, Y):
     new_y = running_median
 
     return np.array(new_x), np.array(new_y), np.array(running_std)
+
+def extract_fix_isophotes(image=None, xcen=None, ycen=None, initsma=None, eps=None, pa=None, step=None, 
+                          linear_growth=False, minsma=None, maxsma=None, silent=False):
+    """
+    Function to extract surface brightness profile with fixed center, ellipticity, and position angle.
+    """
+    syntax = "syntax: results = extract_fix_isophotes(image=, xcen=, ycen=, initsma=, eps=, pa=, step=, linear_growth=False/True, minsma=None, maxsma=None, silent=False/True; minsma maxsma are optional)"
+    
+    from photutils.isophote import (EllipseGeometry, EllipseSample, Isophote,
+                                    IsophoteList)
+    
+    if (None in [xcen, ycen, initsma, eps, pa, step]) or (image is None):
+        print(syntax)
+        return []
+    print(syntax) if silent == False else print("")
+    
+    minsma = minsma if minsma is not None else 0.5
+    maxsma = maxsma if maxsma is not None else max(np.shape(image))/2*1.3
+    isophote_list = []
+
+    geometry = EllipseGeometry(xcen, ycen, initsma, eps, pa, astep=step, linear_growth=False, 
+                               fix_center=True, fix_pa=True, fix_eps=True)
+    
+    sma = initsma
+    while True:
+        sample = EllipseSample(image, sma, geometry=geometry)
+
+        sample.update(geometry.fix)
+        isophote = Isophote(sample, 0, True, stop_code=4)
+        isophote_list.append(isophote)
+        sma = isophote.sample.geometry.update_sma(step)
+        if maxsma and sma >= maxsma:
+            break
+
+    first_isophote = isophote_list[0]
+    sma, step = first_isophote.sample.geometry.reset_sma(step)
+
+    while True:
+        sample = EllipseSample(image, sma, geometry=geometry)
+
+        sample.update(geometry.fix)
+        isophote = Isophote(sample, 0, True, stop_code=4)
+        isophote_list.append(isophote)
+        sma = isophote.sample.geometry.update_sma(step)
+        if minsma and sma <= max(minsma, 0.5):
+            break
+
+    isophote_list.sort()
+    iso_fix = IsophoteList(isophote_list)
+    
+    return iso_fix
 
 
 if __name__ == '__main__':
