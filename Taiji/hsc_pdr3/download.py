66013ed2426166ddeb57645d490e2c160cf4c10c
Dewang Xu <dwxu.astro@gmail.com>
2023-05-13 15:32:42 +0800

    minor

--- Taiji/hsc_pdr3/download.py
+++ Taiji/hsc_pdr3/download.py
@@ -45,7 +45,6 @@ def hsc_cutout_tool(rerun_field,
 
         else:
             print('The {} band cutout does not exist.'.format(filter_band))
-            print('The filename is {}'.format(check_data_file_name))
     print('******************************************')
 
     # check_data_file_name_Iband = data_path + '{0}_{1}_{2}_{3}_{4}_{5:.2f}arcsec_cutout.fits'.format(
@@ -64,14 +63,14 @@ def hsc_cutout_tool(rerun_field,
             .format(rerun_field, data_type_command, coor_ra, coor_dec,
                     size_arcsec, size_arcsec, cutout_download_name)
         ],
-                                    shell=True)
+                                   shell=True)
     elif system_use == 'darwin':
         process = subprocess.Popen([
             "python /Users/xu/Astronomy/Taiji/Taiji/hsc_pdr3/downloadCutout/downloadCutout.py --rerun='{0}' --type='{1}' --mask=True --variance=True --ra={2} --dec={3} --sw={4}arcsec --sh={5}arcsec --name='{6}' --user='dwxu'"
             .format(rerun_field, data_type_command, coor_ra, coor_dec,
                     size_arcsec, size_arcsec, cutout_download_name)
         ],
-                                    shell=True)
+                                   shell=True)
 
     return_code = process.wait()
 
@@ -118,7 +117,7 @@ def hsc_psf_tool(rerun_field,
     #     )
 
     # else:
-        #print(f'We should download the {filter_input} band PSF.')
+    #print(f'We should download the {filter_input} band PSF.')
     os.chdir(data_path)
 
     if system_use == 'linux':
@@ -126,14 +125,14 @@ def hsc_psf_tool(rerun_field,
             "python /home/dewang/Taiji/Taiji/hsc_pdr3/downloadPsf/downloadPsf.py --rerun='{0}' --type='coadd' --ra={1} --dec={2} --name='{3}' --user='dwxu'"
             .format(rerun_field, coor_ra, coor_dec, psf_download_name)
         ],
-                                        shell=True)
+                                       shell=True)
 
     elif system_use == 'darwin':
         process_psf = subprocess.Popen([
             "python /Users/xu/Astronomy/Taiji/Taiji/hsc_pdr3/downloadPsf/downloadPsf.py --rerun='{0}' --type='coadd' --ra={1} --dec={2} --name='{3}' --user='dwxu'"
             .format(rerun_field, coor_ra, coor_dec, psf_download_name)
         ],
-                                        shell=True)
+                                       shell=True)
 
     return_code_psf = process_psf.wait()
 
