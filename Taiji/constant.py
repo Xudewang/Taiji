b6eb6f0a4c43f639f48af4cb1b3b5db4e28a465b
Dewang Xu <dwxu.astro@gmail.com>
2023-06-26 22:26:39 +0800

    update constants

--- Taiji/constant.py
+++ Taiji/constant.py
@@ -1,5 +1,10 @@
 FWHM_f150w = 0.07
 FWHM_f200w = 0.066 # https://jwst-docs.stsci.edu/jwst-near-infrared-camera/nircam-performance/nircam-point-spread-functions#NIRCamPointSpreadFunctions-NIRCamPSFs
+
+# pixel_scale
 pixel_scale_ceers = 0.03 # arcsec/pixel
 pixel_scale_HSCSSP = 0.168 # arcsec/pixel
 pixel_scale_CGS = 0.259 # arcsec/pixel
+
+#zpt0
+zpt0_ceers = 28.08652
