976e606ad2fc09d8186ba0b202662334f40aeb3b
Dewang Xu <dwxu.astro@gmail.com>
2024-02-24 22:33:25 +0800

    improve the get_ap_geometry function using curve of growth trapz.

--- Taiji/autoprof_utils.py
+++ Taiji/autoprof_utils.py
@@ -4,20 +4,40 @@ import logging
 import numpy as np
 from astropy.table import Table
 from photutils.isophote import Ellipse as Photutils_Ellipse
-from photutils.isophote import (EllipseGeometry, EllipseSample, Isophote,
-                                IsophoteList)
+from photutils.isophote import EllipseGeometry, EllipseSample, Isophote, IsophoteList
 from scipy.optimize import minimize
 from scipy.stats import iqr, norm
 
 from Taiji.autoprof_SharedFunctions import (
-    AddLogo, Fmode_fluxdens_to_fluxsum_errorprop, LSBImage,
-    PA_shift_convention, SBprof_to_COG_errorprop, _average, _inv_x_to_eps,
-    _inv_x_to_pa, _iso_between, _iso_extract, _scatter, _x_to_eps, _x_to_pa,
-    autocolours, flux_to_mag, flux_to_sb, fluxdens_to_fluxsum_errorprop,
-    mag_to_flux)
+    AddLogo,
+    Fmode_fluxdens_to_fluxsum_errorprop,
+    LSBImage,
+    PA_shift_convention,
+    SBprof_to_COG_errorprop,
+    _average,
+    _inv_x_to_eps,
+    _inv_x_to_pa,
+    _iso_between,
+    _iso_extract,
+    _scatter,
+    _x_to_eps,
+    _x_to_pa,
+    autocolours,
+    flux_to_mag,
+    flux_to_sb,
+    fluxdens_to_fluxsum_errorprop,
+    mag_to_flux,
+)
 from Taiji.constant import pixel_scale_HSCSSP
-from Taiji.imtools import (GrowthCurve, bright_to_mag, get_Rmag, get_Rpercent,
-                           remove_consecutive, symmetry_propagate_err_mu)
+from Taiji.imtools import (
+    GrowthCurve,
+    GrowthCurve_trapz,
+    bright_to_mag,
+    get_Rmag,
+    get_Rpercent,
+    remove_consecutive,
+    symmetry_propagate_err_mu,
+)
 
 
 def Smooth_Mode(v):
@@ -27,7 +47,7 @@ def Smooth_Mode(v):
     scale = iqr(v) / max(1.0, np.log10(len(v)))  # /10
     # Fit the peak of the smoothed histogram
     res = minimize(
-        lambda x: -np.sum(np.exp(-(((v - x) / scale)**2))),
+        lambda x: -np.sum(np.exp(-(((v - x) / scale) ** 2))),
         x0=[start],
         method="Nelder-Mead",
     )
@@ -41,8 +61,10 @@ def Background_Mode(IMG, mask=None):
         mask = np.logical_not(mask)
     else:
         mask = np.ones(IMG.shape, dtype=bool)
-        mask[int(IMG.shape[0] / 5.0):int(4.0 * IMG.shape[0] / 5.0),
-             int(IMG.shape[1] / 5.0):int(4.0 * IMG.shape[1] / 5.0), ] = False
+        mask[
+            int(IMG.shape[0] / 5.0) : int(4.0 * IMG.shape[0] / 5.0),
+            int(IMG.shape[1] / 5.0) : int(4.0 * IMG.shape[1] / 5.0),
+        ] = False
 
     values = IMG[mask].flatten()
 
@@ -67,7 +89,7 @@ def Background_Mode(IMG, mask=None):
     return {
         "background": bkgrnd,
         "background noise": noise,
-        "background uncertainty": uncertainty
+        "background uncertainty": uncertainty,
     }
 
 
@@ -80,7 +102,7 @@ def My_Background_Mode(IMG, mask=None):
         IMG_copy[mask == 1] = np.nan
         values = IMG_copy.flatten()
     else:
-        raise ValueError('The mask is empty!')
+        raise ValueError("The mask is empty!")
 
     if len(values) < 1e5:
         values = IMG.flatten()
@@ -97,6 +119,7 @@ def My_Background_Mode(IMG, mask=None):
         noise = iqr(values, rng=[16, 84]) / 2.0
 
     return {"background": bkgrnd, "background noise": noise}
+
 
 def Isophote_Initialize(IMG, results, options):
     """Fit global elliptical isophote to a galaxy image using FFT coefficients.
@@ -360,148 +383,155 @@ def Isophote_Initialize(IMG, results, options):
         "auxfile initialize": auxmessage,
     }
 
-def get_geometry_ap(ap_prof_name, ap_aux_name, pixel_scale, zpt0):
+
+def get_geometry_ap(ap_prof_name, ap_aux_name, pixel_scale, zpt0, sky_value, sky_rms):
     import os
 
     import pandas as pd
     from scipy.interpolate import interp1d
-    
+
     if os.path.exists(ap_prof_name) and os.path.exists(ap_aux_name):
         with open(ap_aux_name, "r") as file_aux:
             num_pass = 0
             for line in file_aux:
                 if line.startswith("checkfit"):
-                    num_pass = num_pass + 1 if line.strip(
-                    )[-4:] == "pass" else num_pass
+                    num_pass = num_pass + 1 if line.strip()[-4:] == "pass" else num_pass
 
                 if line.startswith("checkfit FFT"):
-                    check_fit_fft_coefficients = 1 if line.strip(
-                    )[-4:] == "pass" else 0
+                    check_fit_fft_coefficients = 1 if line.strip()[-4:] == "pass" else 0
                 if line.startswith("checkfit Light"):
-                    check_fit_light_symmetry = 1 if line.strip(
-                    )[-4:] == "pass" else 0
+                    check_fit_light_symmetry = 1 if line.strip()[-4:] == "pass" else 0
                 if line.startswith("checkfit initial"):
-                    check_fit_initial_fit_compare = 1 if line.strip(
-                    )[-4:] == "pass" else 0
+                    check_fit_initial_fit_compare = (
+                        1 if line.strip()[-4:] == "pass" else 0
+                    )
                 if line.startswith("checkfit isophote"):
-                    check_fit_isophote_variability = 1 if line.strip(
-                    )[-4:] == "pass" else 0
+                    check_fit_isophote_variability = (
+                        1 if line.strip()[-4:] == "pass" else 0
+                    )
 
                 if line.startswith("global ellipticity") and num_pass >= 0:
-                    ell, err_ell, pa, err_pa, rad_pix = np.array(
-                        line.split())[[2, 4, 6, 8, 11]]
+                    ell, err_ell, pa, err_pa, rad_pix = np.array(line.split())[
+                        [2, 4, 6, 8, 11]
+                    ]
                     if err_ell[-1] == ",":
                         err_ell = err_ell[:-1]
                     else:
                         print("error!")
                         err_ell = np.nan
 
-                if line.startswith('background'):
+                if line.startswith("background"):
                     noise = float(line.split()[-2])
-                if line.startswith('central surface'):
+                if line.startswith("central surface"):
                     central_mu = float(line.split()[3])
-                if line.startswith('center'):
+                if line.startswith("center"):
                     center_x = float(line.split()[2])
                     center_y = float(line.split()[5])
-                if line.startswith('option ap_set_center'):
+                if line.startswith("option ap_set_center"):
                     center_x = np.nan
                     center_y = np.nan
 
         data_ap = pd.read_csv(ap_prof_name, header=1)
         data_ap = Table.from_pandas(data_ap)
-        sma_ap = data_ap['R'] / pixel_scale
-        I_ap = data_ap['I']
-        iso_err_ap = data_ap['I_e']
-        ell_ap = data_ap['ellip']
-        ell_err_ap = data_ap['ellip_e']
-        pa_ap = data_ap['pa']
-        pa_err_ap = data_ap['pa_e']
+        sma_ap = data_ap["R"] / pixel_scale
+        I_ap = data_ap["I"]
+        iso_err_ap = data_ap["I_e"]
+        ell_ap = data_ap["ellip"]
+        ell_err_ap = data_ap["ellip_e"]
+        pa_ap = data_ap["pa"]
+        pa_err_ap = data_ap["pa_e"]
 
-        I_err_ap = np.sqrt(iso_err_ap**2 + noise**2 / pixel_scale**4)
+        I_err_ap = np.sqrt(iso_err_ap**2 + sky_rms**2 / pixel_scale**4)
         mu_ap = bright_to_mag(intens=I_ap, texp=1, pixel_size=1, zpt0=zpt0)
         mu_err_ap = symmetry_propagate_err_mu(I_ap, I_err_ap)
-        tflux = data_ap['totflux_direct']
-        maxIsoFlux = np.max(tflux)
 
-        index_above_sigma_temp = (I_ap > 3 * I_err_ap)
+        index_above_sigma_temp = I_ap > 3 * I_err_ap
 
-        index_above_sigma_fix = remove_consecutive(sma_ap,
-                                                   index_above_sigma_temp)[0]
+        index_above_sigma_fix = remove_consecutive(sma_ap, index_above_sigma_temp)[0]
 
         # calculate the CoG by myself
-        # tflux_ap_me, maxIsoSma_ap_me, maxIsoFlux_ap_me = GrowthCurve(
-        #     sma_ap[index_above_sigma_fix], ell_ap[index_above_sigma_fix],
-        #     I_ap[index_above_sigma_fix] * pixel_scale**2)
+        tflux = GrowthCurve_trapz(
+            R=sma_ap[index_above_sigma_fix],
+            I=ell_ap[index_above_sigma_fix],
+            axisratio=I_ap[index_above_sigma_fix] * pixel_scale**2,
+        )
+
+        maxIsoFlux = np.max(tflux)
 
         # get the Rpercent radius and use this to calculate the average ellipticity and PA.
-        r20 = get_Rpercent(sma_ap, tflux, maxIsoFlux, 0.2)
-        r50 = get_Rpercent(sma_ap, tflux, maxIsoFlux, 0.5)
-        r80 = get_Rpercent(sma_ap, tflux, maxIsoFlux, 0.8)
-        r90 = get_Rpercent(sma_ap, tflux, maxIsoFlux, 0.9)
-        r95 = get_Rpercent(sma_ap, tflux, maxIsoFlux, 0.95)
+        r20 = get_Rpercent(sma_ap[index_above_sigma_fix], tflux, maxIsoFlux, 0.2)
+        r50 = get_Rpercent(sma_ap[index_above_sigma_fix], tflux, maxIsoFlux, 0.5)
+        r80 = get_Rpercent(sma_ap[index_above_sigma_fix], tflux, maxIsoFlux, 0.8)
+        r90 = get_Rpercent(sma_ap[index_above_sigma_fix], tflux, maxIsoFlux, 0.9)
+        r95 = get_Rpercent(sma_ap[index_above_sigma_fix], tflux, maxIsoFlux, 0.95)
 
         R25 = get_Rmag(sma_ap, mu=mu_ap, mag_use=25)
 
         # get the surface brightness at rad_pix, need to interpolate the sma versus mu
-        mu_interp = interp1d(sma_ap, mu_ap, kind='linear')
+        mu_interp = interp1d(sma_ap, mu_ap, kind="linear")
         mu_rad_pix = mu_interp(rad_pix)
 
         # get the ellipticity at different radius, need to interpolate the sma versus ell
-        ell_interp = interp1d(sma_ap, ell_ap, kind='linear')
-        pa_interp = interp1d(sma_ap, pa_ap, kind='linear')
+        ell_interp = interp1d(sma_ap, ell_ap, kind="linear")
+        pa_interp = interp1d(sma_ap, pa_ap, kind="linear")
         ell_R25 = ell_interp(R25)
         pa_R25 = pa_interp(R25)
-        
+
         # derive the average ellipticity and PA within k1 and k2 times r90.
         k1 = 0.675
         k2 = 1.467
         innerRad = k1 * r90
         outRad = k2 * r90
-        
-        ell_avg = np.mean(ell_ap[np.logical_and(sma_ap<outRad, sma_ap>innerRad)])
-        ell_avg_err = np.sqrt(np.sum(ell_err_ap[np.logical_and(sma_ap<outRad, sma_ap>innerRad)]**2))
-        pa_avg = np.mean(pa_ap[np.logical_and(sma_ap<outRad, sma_ap>innerRad)])
-        pa_avg_err = np.sqrt(np.sum(pa_err_ap[np.logical_and(sma_ap<outRad, sma_ap>innerRad)]**2))
+
+        ell_avg = np.mean(ell_ap[np.logical_and(sma_ap < outRad, sma_ap > innerRad)])
+        ell_avg_err = np.sqrt(
+            np.sum(ell_err_ap[np.logical_and(sma_ap < outRad, sma_ap > innerRad)] ** 2)
+        )
+        pa_avg = np.mean(pa_ap[np.logical_and(sma_ap < outRad, sma_ap > innerRad)])
+        pa_avg_err = np.sqrt(
+            np.sum(pa_err_ap[np.logical_and(sma_ap < outRad, sma_ap > innerRad)] ** 2)
+        )
 
     else:
-        print('No such file!')
+        print("No such file!")
         return None
 
     # save all the information to pandas dataframe
-    
+
     data_save = {
-        'center_x': center_x,
-        'center_y': center_y,
-        'r20': r20,
-        'r50': r50,
-        'r80': r80,
-        'r90': r90,
-        'r95': r95,
-        'R25': R25,
-        'ell_R25': ell_R25,
-        'pa_R25': pa_R25,
-        'ell_avg': ell_avg,
-        'ell_avg_err': ell_avg_err,
-        'pa_avg': pa_avg,
-        'pa_avg_err': pa_avg_err,
-        'ell': ell,
-        'err_ell': err_ell,
-        'pa': pa,
-        'err_pa': err_pa,
-        'rad_pix': rad_pix,
-        'check_fit_fft_coefficients': check_fit_fft_coefficients,
-        'check_fit_light_symmetry': check_fit_light_symmetry,
-        'check_fit_initial_fit_compare': check_fit_initial_fit_compare,
-        'check_fit_isophote_variability': check_fit_isophote_variability,
-        'number_of_pass': num_pass,
-        'maxIsoFlux': maxIsoFlux,
-        'central_mu': central_mu,
-        'mu_rad_pix': mu_rad_pix,
+        "center_x": center_x,
+        "center_y": center_y,
+        "r20": r20,
+        "r50": r50,
+        "r80": r80,
+        "r90": r90,
+        "r95": r95,
+        "R25": R25,
+        "ell_R25": ell_R25,
+        "pa_R25": pa_R25,
+        "ell_avg": ell_avg,
+        "ell_avg_err": ell_avg_err,
+        "pa_avg": pa_avg,
+        "pa_avg_err": pa_avg_err,
+        "ell": ell,
+        "err_ell": err_ell,
+        "pa": pa,
+        "err_pa": err_pa,
+        "rad_pix": rad_pix,
+        "check_fit_fft_coefficients": check_fit_fft_coefficients,
+        "check_fit_light_symmetry": check_fit_light_symmetry,
+        "check_fit_initial_fit_compare": check_fit_initial_fit_compare,
+        "check_fit_isophote_variability": check_fit_isophote_variability,
+        "number_of_pass": num_pass,
+        "maxIsoFlux": maxIsoFlux,
+        "central_mu": central_mu,
+        "mu_rad_pix": mu_rad_pix,
     }
 
     df_save = pd.DataFrame([data_save], index=[0])
 
     return df_save
+
 
 def _Generate_Profile(IMG, results, Radius, parameters, options):
 
@@ -585,12 +615,16 @@ def _Generate_Profile(IMG, results, Radius, parameters, options):
                     else 5
                 ),
                 sigmaclip=options["ap_isoclip"] if "ap_isoclip" in options else False,
-                sclip_iterations=options["ap_isoclip_iterations"]
-                if "ap_isoclip_iterations" in options
-                else 10,
-                sclip_nsigma=options["ap_isoclip_nsigma"]
-                if "ap_isoclip_nsigma" in options
-                else 5,
+                sclip_iterations=(
+                    options["ap_isoclip_iterations"]
+                    if "ap_isoclip_iterations" in options
+                    else 10
+                ),
+                sclip_nsigma=(
+                    options["ap_isoclip_nsigma"]
+                    if "ap_isoclip_nsigma" in options
+                    else 5
+                ),
             )
         else:
             isisophoteband = True
@@ -603,27 +637,35 @@ def _Generate_Profile(IMG, results, Radius, parameters, options):
                 mask=mask,
                 more=True,
                 sigmaclip=options["ap_isoclip"] if "ap_isoclip" in options else False,
-                sclip_iterations=options["ap_isoclip_iterations"]
-                if "ap_isoclip_iterations" in options
-                else 10,
-                sclip_nsigma=options["ap_isoclip_nsigma"]
-                if "ap_isoclip_nsigma" in options
-                else 5,
+                sclip_iterations=(
+                    options["ap_isoclip_iterations"]
+                    if "ap_isoclip_iterations" in options
+                    else 10
+                ),
+                sclip_nsigma=(
+                    options["ap_isoclip_nsigma"]
+                    if "ap_isoclip_nsigma" in options
+                    else 5
+                ),
             )
         isotot = np.sum(
             _iso_between(dat, 0, Radius[i], parameters[i], results["center"], mask=mask)
         )
         medflux = _average(
             isovals[0],
-            options["ap_isoaverage_method"]
-            if "ap_isoaverage_method" in options
-            else "median",
+            (
+                options["ap_isoaverage_method"]
+                if "ap_isoaverage_method" in options
+                else "median"
+            ),
         )
         scatflux = _scatter(
             isovals[0],
-            options["ap_isoaverage_method"]
-            if "ap_isoaverage_method" in options
-            else "median",
+            (
+                options["ap_isoaverage_method"]
+                if "ap_isoaverage_method" in options
+                else "median"
+            ),
         )
         if (
             "ap_iso_measurecoefs" in options
@@ -791,9 +833,9 @@ def _Generate_Profile(IMG, results, Radius, parameters, options):
     )
     SBprof_data["pixels"] = list(pixels)
     SBprof_data["maskedpixels"] = list(maskedpixels)
-    SBprof_data[
-        "totflux_direct" if fluxunits == "intensity" else "totmag_direct"
-    ] = list(cogdirect)
+    SBprof_data["totflux_direct" if fluxunits == "intensity" else "totmag_direct"] = (
+        list(cogdirect)
+    )
 
     if "ap_iso_measurecoefs" in options and not options["ap_iso_measurecoefs"] is None:
         whichcoefs = [0] + list(options["ap_iso_measurecoefs"])
@@ -823,6 +865,7 @@ def _Generate_Profile(IMG, results, Radius, parameters, options):
 
     return {"prof header": params, "prof units": SBprof_units, "prof data": SBprof_data}
 
+
 def Isophote_Extract_Photutils(IMG, results, options):
     """Wrapper of photutils method for extracting SB profiles.
 
@@ -841,21 +884,21 @@ def Isophote_Extract_Photutils(IMG, results, options):
     ap_fluxunits : str, default "mag"
       units for outputted photometry. Can either be "mag" for log
       units, or "intensity" for linear units.
-    
+
     ap_plot_sbprof_ylim : tuple, default None
       Tuple with axes limits for the y-axis in the SB profile
       diagnostic plot. Be careful when using intensity units
       since this will change the ideal axis limits.
-    
+
     ap_plot_sbprof_xlim : tuple, default None
       Tuple with axes limits for the x-axis in the SB profile
       diagnostic plot.
-    
+
     ap_plot_sbprof_set_errscale : float, default None
       Float value by which to scale errorbars on the SB profile
       this makes them more visible in cases where the statistical
       errors are very small.
-    
+
     Notes
     ----------
     :References:
